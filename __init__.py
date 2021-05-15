5c4270f109af40955f339cf4a1246c670587acc0
Kevo <39010496+CWKevo@users.noreply.github.com>
2021-05-15 14:08:31 +0200

    Endless conversation

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+
 ROOT = Path(__file__).parent.absolute()
 sys.path.append(ROOT)
 
@@ -7,4 +8,7 @@ sys.path.append(ROOT)
 if __name__ == "__main__":
     from pychatteringy.classes.chatbot import ChatBot
     chatbot = ChatBot(intents_directory="intents")
-    chatbot.parse_intents()
+
+    while True:
+    	x = chatbot.get_response(input("You: "))
+    	print("Bot:", x)
